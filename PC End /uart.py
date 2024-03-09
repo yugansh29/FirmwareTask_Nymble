82f06409a256daecaaaf5ac4a4679c05f795a71a
Yugansh Kumar <yugansh2910@gmail.com>
2024-03-10 00:23:16 +0530

    new

--- PC End /uart.py
+++ PC End /uart.py
@@ -1,7 +1,6 @@
 import serial
 import time
 
-# Open the serial port. Replace 'COM3' with the correct port for your system.
 ser = serial.Serial('COM3', 2400)
 
 start_time = time.time()
@@ -13,9 +12,9 @@ while True:
         end_time = time.time()
         
         elapsed_time = end_time - start_time
-        size_in_bits = len(line) * 8  # each character is 1 byte = 8 bits
-        speed = size_in_bits / elapsed_time if elapsed_time > 0 else 0
+        size_in_bits = len(line)   
+        speed = size_in_bits / elapsed_time
 
         #print(f"Data: {line}")
-        print(f"Transmission speed: {speed} bits/second")
+        print(f"speed: {speed} bits/second")
         time.sleep(1)
